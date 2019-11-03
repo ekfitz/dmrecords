470bba94fc035c0a308c89151ad1a783fb43dae4
Eric <57112072+ekfitz@users.noreply.github.com>
2019-11-02 22:19:57 -0700

    Toggle and hover nav

--- js/main.js
+++ js/main.js
@@ -1,18 +1,74 @@
 window.addEventListener("resize", setMobile);
 
 var hugePic = document.getElementById("hugePic");
-hugePic.src = "images/drums crop.png";
+hugePic.src = "images/logoPNG.png";
+
+var nav1 = document.getElementById("nav1");
+var nav2 = document.getElementById("nav2");
+var nav3 = document.getElementById("nav3");
+var nav4 = document.getElementById("nav4");
+
+var ntxt1 = document.getElementById("ntxt1");
+var ntxt2 = document.getElementById("ntxt2");
+var ntxt3 = document.getElementById("ntxt3");
+var ntxt4 = document.getElementById("ntxt4");
+
+var hmenu = document.getElementById("hmenu");
+var hline1 = document.getElementById("hline1");
+var hline2 = document.getElementById("hline2");
+var hline3 = document.getElementById("hline3");
+
+nav4.onmouseover = highMenu;
+nav4.onmouseout = regMenu;
+nav4.onclick = showMenu;
+
+ntxt1.innerHTML = "ABOUT";
+ntxt2.innerHTML = "CALENDAR";
+ntxt3.innerHTML = "CONTACT";
+
+
+function highMenu() {
+  hline1.style.backgroundColor = "#BD52DB";
+  hline2.style.backgroundColor = "#BD52DB";
+  hline3.style.backgroundColor = "#BD52DB";
+}
+
+function regMenu() {
+  hline1.style.backgroundColor = "white";
+  hline2.style.backgroundColor = "white";
+  hline3.style.backgroundColor = "white";
+}
+
+function showMenu() {
+  if (ntxt1.style.display == "none") {
+    $(".navtext").css('display', 'block');
+  } else {
+    $(".navtext").css('display', 'none');
+  }
+}
+
 function setMobile() {
+  //hide menu
+  $(".navtext").css('display', 'none');
+
+
   var w = window.innerWidth
   || document.documentElement.clientWidth
   || document.body.clientWidth;
 
   if (w > 800) {
     // set desktop
-    $(".headerbox").css('backgroundColor', 'black');
+    $(".navbox").css('width', '10%');
+    nav1.style.left = "50%";
+    nav2.style.left = "62.5%";
+    nav3.style.left = "75%";
+    nav4.style.left = "90%";
   } else {
     // set mobile
-    $(".headerbox").css('backgroundColor', 'blue');
-
+    $(".navbox").css('width', '25%');
+    nav1.style.left = "0.5%";
+    nav2.style.left = "25.5%";
+    nav3.style.left = "50.5%";
+    nav4.style.left = "75.5%";
   }
 }
